332c2fcac79c356e1aabefe13165e87a06450923
shrapk2 <kbsharpton@gmail.com>
2020-04-22 15:15:10 -0500

    Initial commit and functional code sample.

---
+++ README.md
@@ -0,0 +1,35 @@
+# poc-demo1
+
+Proof of Concept demonstration.
+
+## Prerequisites
+
+This Python script parses 2 words and determines if they are anagrams.
+
+```bash
+$ ./poc-demo1.py meteor remote
+True
+$ ./poc-demo1.py meteor word
+False
+```
+
+## Installing
+
+Simply `git clone` the Python executable and go crazy. Or just download the `poc-demo1.py` script, no clone required.
+
+## Built With
+
+- Python
+
+## Authors
+
+- shrapk2
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+- OSS Community and Projects
+- [Google Search](https://www.google.com)
---
+++ poc-demo1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import argparse
+import json
+
+
+# Parse through comnand arguments and do stuff
+def read_command_args():
+    parser = argparse.ArgumentParser(
+        description='Test for anagram awesomeness')
+    parser.add_argument('words', type=str, nargs=2, action='store',
+                        help='Provide 2 words to test for anagram-ness. Any additional words will be ignored!')
+    parser.add_argument('--version', '-v', action='version',
+                        version='%(prog)s 0.1')
+    args, unknown = parser.parse_known_args()
+
+    if anagram_check(args.words[0], args.words[1]):
+        print('True')
+    else:
+        print('False')
+
+
+# Check 2 words as anagrams, return true/false
+def anagram_check(first, second):
+    if(sorted(first) == sorted(second)):
+        return True
+    else:
+        return False
+
+
+# AWS Lambda Compat
+def lambda_handler(event, context):
+    if anagram_check(event[0], event[1]):
+        return {
+            'statusCode': 200,
+            'body': json.dumps('True')
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': json.dumps('False')
+        }
+
+
+# Starts the party [if CLI]
+if __name__ == '__main__':
+    read_command_args()
